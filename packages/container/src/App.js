e6c9e3da5058b5e2f6edd40e499d8cb394cc5d14
karineagredo <karina.a182@gmail.com>
2023-06-05 14:43:12 +1000

    thirsty

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 const App = () => {
-  console.log('test');
+  console.log('thirst');
   return (
     <>
       <MarketingApp></MarketingApp>
