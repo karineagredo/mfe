05b66b12a64a74252f151d592082110967812de9
karineagredo <karina.a182@gmail.com>
2023-06-05 20:21:50 +1000

    Added header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 const App = () => {
   console.log('6');
   return (
-    <>
-      <MarketingApp></MarketingApp>
-    </>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp></MarketingApp>
+      </div>
+    </BrowserRouter>
   );
 };
 
