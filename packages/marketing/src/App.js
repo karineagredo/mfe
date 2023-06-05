b5b0cef0b0c4fa126baf3e02add6bbf321b23520
karineagredo <karina.a182@gmail.com>
2023-06-06 09:38:22 +1000

    random classes added in Marketing app

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-const Test = () => <div>test component</div>;
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'marketing',
+});
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing}></Route>
